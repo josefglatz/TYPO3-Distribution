9d80e665515a521a884827db9a52f90b80128de6
Josef Glatz <jousch@gmail.com>
2016-09-02 14:44:05 +0200

    [TASK] AdditionalConfiguration: add for Development/Docker

---
+++ app/web/typo3conf/AdditionalConfiguration_Development_Docker.php
@@ -0,0 +1,31 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+// Override configuration of LocalConfiguration
+$customChanges = [
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'dbname' => getenv('MYSQL_DATABASE'),
+                'driver' => 'mysqli',
+                'host' => 'mysql',
+                'password' => getenv('MYSQL_PASSWORD'),
+                'port' => 3306,
+                'user' => getenv('MYSQL_USER'),
+            ],
+        ],
+    ],
+    'GFX' => [
+        'colorspace' => 'RGB',
+        'processor' => 'GraphicsMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => -1,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+        'processor_path_lzw' => '/usr/bin/',
+    ],
+];
+
+$GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive($GLOBALS['TYPO3_CONF_VARS'], $customChanges);
