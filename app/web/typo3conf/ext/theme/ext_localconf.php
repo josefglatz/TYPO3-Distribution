b1e5264a2611d26ba2e8f4e8bcbb7844e8593c3f
Josef Glatz <jousch@gmail.com>
2016-09-06 06:48:32 +0200

    [TASK] Activate general UserTSConfig

--- app/web/typo3conf/ext/theme/ext_localconf.php
+++ app/web/typo3conf/ext/theme/ext_localconf.php
@@ -11,6 +11,12 @@ call_user_func(
 //        unset($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['news']);
 
 //        @TODO: RealUrl Hooks for disabling cache and so on
+
+
+        // Add general UserTSConfig
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+            '<INCLUDE_TYPOSCRIPT: source="FILE: EXT:' . $extKey . '/Configuration/TSConfig/UserGeneral.tsc">'
+        );
     },
     $_EXTKEY
 );
