6625b38f1145e8d441792a11784508894a95f4d9
Josef Glatz <jousch@gmail.com>
2016-09-06 09:34:27 +0200

    [WIP][TASK] Add RTE PageTSConfig
    mimimi

--- app/web/typo3conf/AdditionalConfiguration.php
+++ app/web/typo3conf/AdditionalConfiguration.php
@@ -53,6 +53,15 @@ $customChanges = [
                 'autoConfFormat' => 0,
                 'enableDevLog' => 0,
             ]),
+            'rtehtmlarea' => serialize([
+                'defaultConfiguration' => 'Minimal',
+                'enableInlineElements' => 0,
+                'allowStyleAttribute' => 0,
+                'enableAccessibilityIcons' => 0,
+                'forceCommandMode' => 0,
+                'noSpellCheckLanguages' => 'ja,km,ko,lo,th,zh,b5,gb',
+                'AspellDirectory' => '/usr/bin/aspell',
+            ]),
 //            'lfeditor' => serialize([
 //                'viewLanguages' => 'de',
 //                'defaultLanguage' => '',
