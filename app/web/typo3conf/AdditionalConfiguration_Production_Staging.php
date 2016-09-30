8a407d542492b5dc87f5b6f8bcaf92ae4d6b34d9
Josef Glatz <jousch@gmail.com>
2016-09-30 07:58:17 +0200

    [TASK] Add skeleton for applicationContext specific DB settings

---
+++ app/web/typo3conf/AdditionalConfiguration_Production_Staging.php
@@ -0,0 +1,21 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+// Override configuration of LocalConfiguration
+$customChanges = [
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'dbname' => '',
+                'driver' => 'mysqli',
+                'host' => '',
+                'password' => '',
+                'port' => 3306,
+                'user' => '',
+            ],
+        ],
+    ],
+];
+
+$GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive($GLOBALS['TYPO3_CONF_VARS'], $customChanges);
