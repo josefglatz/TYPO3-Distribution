f3eebc7b3c22e95dcc1486dc4308d3a199e5c123
Josef Glatz <jousch@gmail.com>
2016-09-30 16:11:37 +0200

    [TASK] Migrate FalViewHelper TYPO3 db wrapper to Doctrine DBAL syntax
    Thx to Jan for the nice blog post about migrating:
    http://jans-blog.helke.de/2016/04/14/migrate-from-the-typo3-database-wrapper-to-the-doctrine-dbal-syntax/
    
    Add also simple example usage of the viewhelper.
    
    Related: #9

--- app/web/typo3conf/ext/theme/Classes/ViewHelpers/FalViewHelper.php
+++ app/web/typo3conf/ext/theme/Classes/ViewHelpers/FalViewHelper.php
@@ -1,13 +1,35 @@
 <?php
 namespace Sup7even\Theme\ViewHelpers;
 
-use TYPO3\CMS\Core\Database\DatabaseConnection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3\CMS\Frontend\Resource\FileCollector;
 
+
 /**
  * Class FalViewHelper
+ *
+ * = Example =
+ *
+ * <code title="register namespace in fluid first">
+ * xmlns:theme="http://typo3.org/ns/Vendor/Theme/ViewHelpers"
+ * </code>
+ *
+ * <code title="default notation">
+ * <theme:fal table="pages" field="image" id="{row.uid}" as="references">
+ * <f:if condition="{references}">
+ *  <f:then>
+ *    <f:media file="{references.0}" class="foobar" title="{references.0.propertiesOfFileReference.title}"/>
+ *  </f:then>
+ *  <f:else>
+ *    <img class="dummy" src="https://dummyimage.com/600x600/444/fff" alt="">
+ *  </f:else>
+ * </f:if>
+ * </theme:fal>
+ * </code>
+ *
  */
 class FalViewHelper extends AbstractViewHelper
 {
@@ -26,8 +48,15 @@ class FalViewHelper extends AbstractViewHelper
      */
     public function render(string $table, string $field, string $id, string $as = 'references')
     {
-        // @TODO: Doctrine dbal rewrite
-        $row = $this->getDatabaseConnection()->exec_SELECTgetSingleRow('*', $table, 'uid=' . (int)$id);
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable($table);
+        $row = $queryBuilder
+            ->select('*')
+            ->from($table)
+            ->where('uid=' . (int)$id)
+            ->execute()
+            ->fetch();
         if (!$row) {
             return '';
         }
@@ -40,15 +69,6 @@ class FalViewHelper extends AbstractViewHelper
         $this->templateVariableContainer->remove($as);
 
         return $output;
-    }
-
-    /**
-     * @return DatabaseConnection
-     */
-    protected function getDatabaseConnection()
-    {
-        // @TODO: Doctrine dbal rewrite
-        return $GLOBALS['TYPO3_DB'];
     }
 
 
