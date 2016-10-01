19391e18254852b09170333622b2ef4b6b1827fc
Josef Glatz <jousch@gmail.com>
2016-10-01 09:27:15 +0200

    [TASK] TCA: refactor TCA/Overrides files
    wrap code into `call_user_func()`

--- app/web/typo3conf/ext/theme/Configuration/TCA/Overrides/sys_template.php
+++ app/web/typo3conf/ext/theme/Configuration/TCA/Overrides/sys_template.php
@@ -2,17 +2,26 @@
 
 defined('TYPO3_MODE') || die ('Access denied.');
 
-/**
- * Embed static TypoScript template(s)
- * by adding all your available sites to $websites
- *
- * e.g.: siteDefault = EXT:theme/Configuration/TypoScript/Sites/SiteDefault
- */
-$websites = ['SiteDefault'];
-foreach($websites as $website) {
-    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        'theme',
-        'Configuration/TypoScript/Sites/' . $website,
-        'Thm TS:' . $website
-    );
-}
+call_user_func(
+    function ($extKey) {
+
+
+        /**
+         * Embed static TypoScript template(s)
+         * by adding all your available sites to $websites
+         *
+         * e.g.: siteDefault = EXT:theme/Configuration/TypoScript/Sites/SiteDefault
+         */
+        $websites = ['SiteDefault'];
+        foreach ($websites as $website) {
+            TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+                'theme',
+                'Configuration/TypoScript/Sites/'.$website,
+                'Thm TS:'.$website
+            );
+        }
+
+
+    },
+    'theme'
+);
