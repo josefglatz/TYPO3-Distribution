3be7416d04a1c52e4e8001a205cc687218505ed2
Josef Glatz <jousch@gmail.com>
2016-10-03 10:41:09 +0200

    [DOCUMENTATION] Add info about realurl autoConf prepared hook

--- app/web/typo3conf/ext/theme/Resources/Private/Extension/Realurl/ManualConfiguration.php
+++ app/web/typo3conf/ext/theme/Resources/Private/Extension/Realurl/ManualConfiguration.php
@@ -2,6 +2,6 @@
 /**
  * Use this file for adding your manual realurl configuration.
  *
- * Really need that? Take a look at the autoConfiguration hook, which realurl offers!
+ * Really need that? Take a look at the autoConfiguration hook, which realurl offers! (ext:theme/Classes/Hooks/Frontend/Realurl/RealUrlAutoConfiguration.php)
  * `$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']`
  */
