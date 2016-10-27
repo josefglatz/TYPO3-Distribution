c7e84a3fd49b41f77cef84c2be5abb5c2284a06e
Josef Glatz <jousch@gmail.com>
2016-10-27 07:16:17 +0200

    [TASK] AdditionalFactoryConfiguration: common factory configuration override

---
+++ app/web/typo3conf/AdditionalFactoryConfiguration.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+* This file is part of the TYPO3 CMS project.
+*
+* It is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License, either version 2
+* of the License, or any later version.
+*
+* For the full copyright and license information, please read the
+* LICENSE.txt file that was distributed with this source code.
+*
+* The TYPO3 project - inspiring people to share!
+*/
+
+/**
+ * This is a boilerplate of typo3conf/AdditionalFactoryConfiguration.php.
+ * It is used as override file during installation and overrides/extends
+ * defaults from EXT:core/Configuration/FactoryConfiguration.php.
+ */
+return [
+    'EXTCONF' => [
+        'lang' => [
+            'availableLanguages' => [
+                'de',
+            ],
+        ],
+    ],
+    'GFX' => [
+        // Override default quality for processed images
+        'jpg_quality' => '86',
+        'processor_allowUpscaling' => false,
+    ],
+    'SYS' => [
+        'sitename' => 'TYPO3 Distribution',
+    ],
+];
