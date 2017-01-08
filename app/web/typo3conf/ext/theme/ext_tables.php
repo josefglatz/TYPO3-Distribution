48f74b165e80b18b0681f79841b6a14c7cb9c81e
Josef Glatz <jousch@gmail.com>
2017-01-08 12:45:10 +0100

    [FEATURE] Backend icons: backend layouts svg icons
    I've vectorized some of the icons provided by [Thomas Deutling](https://github.com/typo3-themes/themes_gridelements/tree/master/Resources/Public/Icons/BackendLayouts)
    
    * add new icons
    * add source ai file
    * use icon identifier for existing beLayouts instead of paths
    
    Resolves: #64

--- app/web/typo3conf/ext/theme/ext_tables.php
+++ app/web/typo3conf/ext/theme/ext_tables.php
@@ -11,18 +11,36 @@ call_user_func(
             // register svg icons: identifier and filename
             $iconsSvg = [
                 'theme-backend-realurl-reset' => 'theme-backend-realurl-reset.svg',
+
                 'theme-content-inheritance-stop' => 'theme-content-inheritance-stop.svg',
-                'theme-content-gridelement' => 'GridElements/theme-content-gridelement.svg',
-                'theme-content-gridelement-2-col' => 'GridElements/theme-content-gridelement-2-col.svg',
-                'theme-content-gridelement-3-col' => 'GridElements/theme-content-gridelement-3-col.svg',
-                'theme-content-gridelement-4-col' => 'GridElements/theme-content-gridelement-4-col.svg',
-                'theme-content-gridelement-accordion' => 'GridElements/theme-content-gridelement-accordion.svg',
-                'theme-content-gridelement-carousel' => 'GridElements/theme-content-gridelement-carousel.svg',
-                'theme-content-gridelement-container' => 'GridElements/theme-content-gridelement-container.svg',
+
+                'theme-content-gridelement'                             => 'GridElements/theme-content-gridelement.svg',
+                'theme-content-gridelement-2-col'                       => 'GridElements/theme-content-gridelement-2-col.svg',
+                'theme-content-gridelement-3-col'                       => 'GridElements/theme-content-gridelement-3-col.svg',
+                'theme-content-gridelement-4-col'                       => 'GridElements/theme-content-gridelement-4-col.svg',
+                'theme-content-gridelement-accordion'                   => 'GridElements/theme-content-gridelement-accordion.svg',
+                'theme-content-gridelement-carousel'                    => 'GridElements/theme-content-gridelement-carousel.svg',
+                'theme-content-gridelement-container'                   => 'GridElements/theme-content-gridelement-container.svg',
                 'theme-content-gridelement-single-column-header-footer' => 'GridElements/theme-content-gridelement-single-column-header-footer.svg',
-                'theme-content-gridelement-single-column-horizontal' => 'GridElements/theme-content-gridelement-single-column-horizontal.svg',
-                'theme-content-gridelement-single-column-vertical' => 'GridElements/theme-content-gridelement-single-column-vertical.svg',
-                'theme-content-gridelement-tab' => 'GridElements/theme-content-gridelement-tab.svg',
+                'theme-content-gridelement-single-column-horizontal'    => 'GridElements/theme-content-gridelement-single-column-horizontal.svg',
+                'theme-content-gridelement-single-column-vertical'      => 'GridElements/theme-content-gridelement-single-column-vertical.svg',
+                'theme-content-gridelement-tab'                         => 'GridElements/theme-content-gridelement-tab.svg',
+
+                'theme-belayout-content'                => 'BackendLayouts/content.svg',
+                'theme-belayout-content-empty'          => 'BackendLayouts/content-empty.svg',
+                'theme-belayout-content-homepage'       => 'BackendLayouts/content-homepage.svg',
+                'theme-belayout-content-menu'           => 'BackendLayouts/content-menu.svg',
+                'theme-belayout-content-menu-sidebar'   => 'BackendLayouts/content-menu-sidebar.svg',
+                'theme-belayout-content-sidebar'        => 'BackendLayouts/content-sidebar.svg',
+                'theme-belayout-content-sidebar-menu'   => 'BackendLayouts/content-sidebar-menu.svg',
+                'theme-belayout-content-special'        => 'BackendLayouts/content-special.svg',
+                'theme-belayout-default'                => 'BackendLayouts/default.svg',
+                'theme-belayout-menu-content'           => 'BackendLayouts/menu-content.svg',
+                'theme-belayout-menu-content-sidebar'   => 'BackendLayouts/menu-content-sidebar.svg',
+                'theme-belayout-menu-sidebar-content'   => 'BackendLayouts/menu-sidebar-content.svg',
+                'theme-belayout-sidebar-content'        => 'BackendLayouts/sidebar-content.svg',
+                'theme-belayout-sidebar-content-menu'   => 'BackendLayouts/sidebar-content-menu.svg',
+                'theme-belayout-sidebar-menu-content'   => 'BackendLayouts/sidebar-menu-content.svg',
             ];
             $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
             foreach ($iconsSvg as $identifier => $path) {
