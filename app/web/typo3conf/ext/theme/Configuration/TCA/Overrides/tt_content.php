d0fa332fc4a35677c987af142352a7384e5ed2fd
Josef Glatz <jousch@gmail.com>
2017-02-08 08:54:33 +0100

    [BUGFIX] Temporarily remove predefined image manipulation crop ratios
    due the changes in the TYPO3 core https://github.com/TYPO3/TYPO3.CMS/commit/063938393d793d7e4b0261da8fe709073a815ae9

--- app/web/typo3conf/ext/theme/Configuration/TCA/Overrides/tt_content.php
+++ app/web/typo3conf/ext/theme/Configuration/TCA/Overrides/tt_content.php
@@ -6,20 +6,6 @@ call_user_func(
         $languageFileCePrefix = 'LLL:EXT:theme/Resources/Private/Language/locallang_ContentElements.xlf:';
 
         /**
-         * set common image crop ratios
-         *
-         * - 3x2
-         * - 4x3
-         * - NaN/free
-         */
-        $GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config']['ratios'] = [
-            '1.5' => '3 x 2',
-            '1.333333333' => '4 x 3',
-            'NaN' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.free',
-        ];
-
-
-        /**
          * Add subheader property to content elements
          * where it makes basically sense
          *
