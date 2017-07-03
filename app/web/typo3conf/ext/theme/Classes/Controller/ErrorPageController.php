e21d6c114a4f5af4b0e99eb39240b28a10d3c1f0
Georg Ringer <georg.ringer@gmail.com>
2017-07-03 20:44:13 +0200

    [CLEANUP] Use correct header

--- app/web/typo3conf/ext/theme/Classes/Controller/ErrorPageController.php
+++ app/web/typo3conf/ext/theme/Classes/Controller/ErrorPageController.php
@@ -3,17 +3,9 @@ declare(strict_types=1);
 
 namespace JosefGlatz\Theme\Controller;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
+/**
+ * This file is part of the "theme" Extension which is part of
+ * the jousch/TYPO3-Distribution for TYPO3 CMS.
  */
 
 use TYPO3\CMS\Core\Messaging\AbstractMessage;
