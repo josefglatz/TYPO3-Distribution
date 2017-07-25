d7c9ae21cb27c8647eedd301992fd3123e202688
Georg Ringer <georg.ringer@gmail.com>
2017-07-25 08:13:38 +0200

    [!!!][TASK] Load the TypoScript by a hook instead of include static
    The TS can be included by a hook which avoid the required
    setup of a template record.

---
+++ app/web/typo3conf/ext/theme/Classes/Hooks/Frontend/TypoScriptHook.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the "theme" Extension which is part of
+ * the jousch/TYPO3-Distribution for TYPO3 CMS.
+ */
+
+namespace JosefGlatz\Theme\Hooks\Frontend;
+
+class TypoScriptHook
+{
+
+    /**
+     * Configuration for the various possible TS entry points
+     * Key = page id
+     *
+     * @var array
+     */
+    protected $configuration = [
+        1 => [
+            'path' => 'SiteDefault',
+            'title' => 'Default Theme',
+            'uid' => 'theme_default'
+        ]
+    ];
+
+    /**
+     * Hooks into TemplateService to load the default TS
+     *
+     * @param array $parameters
+     * @param \TYPO3\CMS\Core\TypoScript\TemplateService $parentObject
+     */
+    public function addCustomTypoScriptTemplate($parameters, $parentObject)
+    {
+        // Disable the inclusion of default TypoScript set via TYPO3_CONF_VARS
+//        $parameters['isDefaultTypoScriptAdded'] = true;
+        // Disable the inclusion of ext_typoscript_setup.txt of all extensions
+//        $parameters['processExtensionStatics'] = false;
+
+        // No template was found in rootline so far, so a custom "fake" sys_template record is added
+        if ($parentObject->outermostRootlineIndexWithTemplate === 0) {
+            if (is_array($parameters['rootLine']) && !empty($parameters['rootLine'])) {
+                $rootPage = $parameters['rootLine'][0];
+                $rootPageId = (int)$rootPage['uid'];
+                foreach ($this->configuration as $configurationPageId => $configuration) {
+                    if ($configurationPageId !== $rootPageId) {
+                        continue;
+                    }
+                    $path = 'FILE:EXT:theme/Configuration/TypoScript/Sites/' . $configuration['path'] . '/';
+                    $row = [
+                        'config' => '<INCLUDE_TYPOSCRIPT: source="' . $path . 'setup.typoscript">',
+                        'constants' => '<INCLUDE_TYPOSCRIPT: source="' . $path . 'constants.typoscript">',
+                        'nextLevel' => 0,
+                        'static_file_mode' => 1,
+                        'uid' => $configuration['uid'],
+                        'title' => $configuration['title'],
+                    ];
+                    $parentObject->processTemplate($row, 'sys_' . $row['uid'], $configurationPageId, 'sys_' . $row['uid']);
+                }
+            }
+        }
+        if (!$parentObject->rootId) {
+            $parentObject->rootId = 1;
+        }
+    }
+}
