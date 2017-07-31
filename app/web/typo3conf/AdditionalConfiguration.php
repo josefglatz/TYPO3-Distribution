501810e47a5a20fee9642a53ffd75dc9dc934b01
Josef Glatz <jousch@gmail.com>
2017-07-31 17:13:48 +0200

    [TASK] Update order of extConf items in AdditionalConfiguration.php

--- app/web/typo3conf/AdditionalConfiguration.php
+++ app/web/typo3conf/AdditionalConfiguration.php
@@ -61,6 +61,15 @@ $customChanges = [
     ],
     'EXT' => [
         'extConf' => [
+            'backend' => serialize([
+                'loginLogo' => 'EXT:theme/Resources/Public/Images/Backend/Login/LoginLogo.svg',
+                'loginHighlightColor' => '#ff8700',
+                'loginBackgroundImage' => 'EXT:theme/Resources/Public/Images/Backend/Login/Background.png',
+                'backendLogo' => 'EXT:theme/Resources/Public/Images/Backend/' . $site['backendLogo'] . '.svg', // custom backend logo should not by higher than 37px
+                // @TODO: Backend Logo (Stage, Prod, Dev)
+                'backendFavicon' => 'EXT:backend/Resources/Public/Icons/favicon.ico',
+                // @TODO: Backend FavIcon (Stage, Prod, Dev)
+            ]),
             'be_secure_pw' => serialize([
                 'validUntil' => '6 months', // Period to remind the user (after login) for setting a new password. Please use english (e.g. "14 days")
                 'forcePasswordChange' => 0, // Force changing the password: This disables all modules except user_setup to force a change of the password when the validUntil period is over or the checkbox in the be_user record  is set
@@ -86,28 +95,14 @@ $customChanges = [
                 'mapping_base_directory' => 'EXT:extractor/Configuration/Services/',
                 'mapping_configuration' => ''
             ]),
+            'fluid_styled_content' => serialize([
+                'loadContentElementWizardTsConfig' => 1,
+            ]),
             'gravatar' => serialize([
                 'fallback' => '', // Fallback behaviour: see https://www.gravatar.com/site/implement/images/  // Transparent/blank=, mystery-man=mm, identicon=identicon, monsterid=monsterid, wavatar=wavatar, retro=retro, Use fallback image=url
                 'fallbackImageUrl' => '', // Fallback image url: MUST be publicly available, see https://www.gravatar.com/site/implement/images/
                 'forceProvider' => false, // Use Gravatar service even if email address is empty: Normally the Gravatar service is only requested when a email address is set. With this setting you can also enable is for BE users without email address. The above fallback will always be used then.
                 'useProxy' => true, // Improve privacy by proxying the image request
-            ]),
-            't3monitoring_client' => serialize([
-                'secret' => 'Provide some secret password',
-                'allowedIps' => '2a03:2a00:1100:2::ac10:29bc,172.17.0.1,188.94.251.75',
-                'enableDebugForErrors' => 0,
-            ]),
-            'backend' => serialize([
-                'loginLogo' => 'EXT:theme/Resources/Public/Images/Backend/Login/LoginLogo.svg',
-                'loginHighlightColor' => '#ff8700',
-                'loginBackgroundImage' => 'EXT:theme/Resources/Public/Images/Backend/Login/Background.png',
-                'backendLogo' => 'EXT:theme/Resources/Public/Images/Backend/' . $site['backendLogo'] . '.svg', // custom backend logo should not by higher than 37px
-                // @TODO: Backend Logo (Stage, Prod, Dev)
-                'backendFavicon' => 'EXT:backend/Resources/Public/Icons/favicon.ico',
-                // @TODO: Backend FavIcon (Stage, Prod, Dev)
-            ]),
-            'fluid_styled_content' => serialize([
-                'loadContentElementWizardTsConfig' => 1,
             ]),
             'image_autoresize_ff' => serialize([
                 'directories' => 'fileadmin/,uploads/',
@@ -119,6 +114,12 @@ $customChanges = [
                 'conversion_mapping' => 'ai => jpg,bmp => jpg,pcx => jpg,tga => jpg,tif => jpg,tiff => jpg',
                 'keep_metadata' => 0,
                 'resize_png_with_alpha' => 0,
+            ]),
+            'lfeditor' => serialize([
+                'viewLanguages' => '', // cz,de,fr,hr,hu,it,nl,pl,ru,sk
+                'defaultLanguage' => '',
+                'extIgnore' => '/^(CVS|.svn|.git|csh_)/',
+                'changeXlfDate' => 1,
             ]),
             'news' => serialize([
                 'archiveDate' => 'date',
@@ -157,12 +158,6 @@ $customChanges = [
                 'facebook_app_id' => '',
                 'facebook_secret' => '',
             ]),
-            'lfeditor' => serialize([
-                'viewLanguages' => '', // cz,de,fr,hr,hu,it,nl,pl,ru,sk
-                'defaultLanguage' => '',
-                'extIgnore' => '/^(CVS|.svn|.git|csh_)/',
-                'changeXlfDate' => 1,
-            ]),
             'saltedpasswords' => serialize([
                 'BE.' => [
                     'saltedPWHashingMethod' => \TYPO3\CMS\Saltedpasswords\Salt\Pbkdf2Salt::class, // Default since 8-7 (for legacy reasons still here)
@@ -189,6 +184,11 @@ $customChanges = [
             ]),
             'static_info_tables' => serialize([
                 'enableManager' => 0,
+            ]),
+            't3monitoring_client' => serialize([
+                'secret' => 'Provide some secret password',
+                'allowedIps' => '2a03:2a00:1100:2::ac10:29bc,172.17.0.1,188.94.251.75',
+                'enableDebugForErrors' => 0,
             ]),
         ],
     ],
