9eee8ba55ad2e65190de1a8343c7cff9a01d227d
Josef Glatz <jousch@gmail.com>
2017-07-31 12:32:04 +0200

    [FEATURE] TrimViewHelper
    Common used VH extracted from ext:vhs.

---
+++ app/web/typo3conf/ext/theme/Classes/ViewHelpers/Format/TrimViewHelper.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the "theme" Extension which is part of
+ * the jousch/TYPO3-Distribution for TYPO3 CMS.
+ */
+
+namespace JosefGlatz\Theme\ViewHelpers\Format;
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithContentArgumentAndRenderStatic;
+
+/**
+ * Trims $content by stripping off $characters (string list
+ * of individual chars to strip off, default is all whitespaces).
+ */
+class TrimViewHelper extends AbstractViewHelper
+{
+    use CompileWithContentArgumentAndRenderStatic;
+
+    /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('content', 'string', 'String to trim');
+        $this->registerArgument('characters', 'string', 'List of characters to trim, no separators, e.g. "abc123"');
+    }
+
+    /**
+     * Trims content by stripping off $characters
+     *
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return mixed
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        $characters = $arguments['characters'];
+        $content = $renderChildrenClosure();
+        if (false === empty($characters)) {
+            $content = trim($content, $characters);
+        } else {
+            $content = trim($content);
+        }
+        return $content;
+    }
+}
