d4a68ae8a9747c18b4765d77bc6a930c9b1c076c
Josef Glatz <jousch@gmail.com>
2017-08-15 12:50:46 +0200

    [WIP][TASK] Add initial common additional db columns

--- app/web/typo3conf/ext/theme/ext_tables.sql
+++ app/web/typo3conf/ext/theme/ext_tables.sql
@@ -1,11 +1,12 @@
 CREATE TABLE tt_content (
-
+  bodytext_1 text,
+  bodytext_2 text
 );
 
 CREATE TABLE pages (
-
+  sharing_enabled int(11) unsigned DEFAULT '1'
 );
 
 CREATE TABLE tx_news_domain_model_news (
-
+  tx_theme_contact int(11) unsigned DEFAULT '0' NOT NULL
 );
