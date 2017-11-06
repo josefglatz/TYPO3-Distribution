0d7ebd3d6f06092a2ceefedda3c961364b53906b
Georg Ringer <georgringer@users.noreply.github.com>
2017-11-06 12:43:17 +0100

    [TASK] Remove not needed fields from ext_tables.sq

--- app/web/typo3conf/ext/theme/ext_tables.sql
+++ app/web/typo3conf/ext/theme/ext_tables.sql
@@ -7,7 +7,3 @@ CREATE TABLE pages (
   tx_theme_sharing_enabled int(11) unsigned DEFAULT '1',
   tx_theme_opengraph_image int(11) unsigned DEFAULT '0' NOT NULL
 );
-
-CREATE TABLE tx_news_domain_model_news (
-  tx_theme_contact int(11) unsigned DEFAULT '0' NOT NULL
-);
