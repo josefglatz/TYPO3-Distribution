96a0ed50d373c673f5d1fb28153cb0d8bf79271c
Josef Glatz <jousch@gmail.com>
2017-12-20 13:02:32 +0100

    [FEATURE] Introduce ViewHelper for removing empty paragraphs
    I am against empty P tags! Editors will love it …not!
    
     _________________________________________
    ( You can not just create gaps with empty )
    ( paragraphs                              )
     -----------------------------------------
            o   ^__^
             o  (oo)\_______
                (__)\       )\/\
                    ||----w |
                    ||     ||

---
+++ app/web/typo3conf/ext/theme/Classes/ViewHelpers/Format/RemoveEmptyParagraphViewHelper.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace JosefGlatz\Theme\ViewHelpers\Format;
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithContentArgumentAndRenderStatic;
+
+/**
+ * Remove empty P tags from $content by using preg_replace
+ */
+class RemoveEmptyParagraphViewHelper extends AbstractViewHelper
+{
+    use CompileWithContentArgumentAndRenderStatic;
+
+    /**
+     * @var bool
+     */
+    protected $escapeOutput = false;
+
+    /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('content', 'string', 'String to be relieved of empty paragraphs');
+    }
+
+    /**
+     * Return $content without empty paragraphs
+     *
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return mixed
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        $content = $renderChildrenClosure();
+        $content = preg_replace('~\\s?<p>(\\s|&nbsp;)+</p>\\s?~', '', $content);
+        return $content;
+    }
+}
