724ab876669e1d4a844b7814a273ed93c5d200ce
Josef Glatz <jousch@gmail.com>
2018-03-07 14:49:21 +0100

    [WIP][FEATURE] cropVariants service: CoverAreaDefaults
    Related: #252

---
+++ app/web/typo3conf/ext/theme/Classes/Utility/CropVariants/CoverAreaDefaults.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types = 1);
+
+namespace JosefGlatz\Theme\Utility\CropVariants;
+
+class CoverAreaDefaults
+{
+    /**
+     * @var array cropArea configuration
+     */
+    protected static $coverAreaPresets = [
+        // 1_1_circle can be used for square image (1:1) with CSS `border-radius: 50%` (Tries to cover edges)
+        '1_1_circle' => [
+            [
+                'x' => 0.0,
+                'y' => 0.0,
+                'width' => 0.25,
+                'height' => 0.25
+            ],
+            [
+                'x' => 0.75,
+                'y' => 0.0,
+                'width' => 0.25,
+                'height' => 0.25
+            ],
+            [
+                'x' => 0.0,
+                'y' => 0.75,
+                'width' => 0.25,
+                'height' => 0.25
+            ],
+            [
+                'x' => 0.75,
+                'y' => 0.75,
+                'width' => 0.25,
+                'height' => 0.25
+            ]
+        ],
+        // Cover lower third of image
+        'lower_third' => [
+            [
+                'x' => 0.75,
+                'y' => 0.0,
+                'width' => 1.0,
+                'height' => 0.25
+            ]
+        ]
+    ];
+
+    /**
+     * Retrieve cover area presets
+     *
+     * @param array $keys
+     * @return array all retrieved cover areas
+     */
+    public static function getCoverAreas(array $keys): array
+    {
+        $coverAreas = [];
+        foreach ($keys as $key) {
+            if (isset(self::$coverAreaPresets['key']) && \is_array(self::$coverAreaPresets['key'])) {
+                $coverAreas[] = self::$coverAreaPresets[$key];
+            } else {
+                throw new \UnexpectedValueException('Given coverArea preset "' . $key . '" not found or not from type array."', 1520430221);
+            }
+        }
+
+        return $coverAreas;
+    }
+}
