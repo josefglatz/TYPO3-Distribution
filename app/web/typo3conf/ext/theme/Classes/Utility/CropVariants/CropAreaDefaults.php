2380f004b403569e28c5e25b9c79684359d79e73
Josef Glatz <jousch@gmail.com>
2018-03-07 14:31:20 +0100

    [WIP][FEATURE] cropVariants service: CropAreaDefaults

---
+++ app/web/typo3conf/ext/theme/Classes/Utility/CropVariants/CropAreaDefaults.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+namespace JosefGlatz\Theme\Utility\CropVariants;
+
+class CropAreaDefaults
+{
+    /**
+     * @var array cropArea configuration
+     */
+    protected static $cropAreas = [
+        'default' => [
+            'x' => '0.0',
+            'y' => '0.0',
+            'width' => '1.0',
+            'height' => '1.0'
+        ],
+    ];
+
+    /**
+     * Retrieve crop area
+     *
+     * @param string $name
+     * @return array cropArea (default cropArea if no parameter was hand over)
+     */
+    public static function getCropArea(string $name = 'default'): array
+    {
+        if (isset(self::$cropAreas[$name]) && \is_array(self::$cropAreas[$name])) {
+            return self::$cropAreas[$name];
+        }
+        throw new \UnexpectedValueException('Given crop area "' . $name . '" not found."', 1520429257);
+    }
+}
