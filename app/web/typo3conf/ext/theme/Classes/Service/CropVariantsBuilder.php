fc5a2f81ecaedc0b1f4367b769adc25543731ed8
Josef Glatz <jousch@gmail.com>
2018-03-08 05:52:12 +0100

    [WIP][FEATURE] cropVariants service: add changes in CropVariantsBuilder service
    Related: #252

--- app/web/typo3conf/ext/theme/Classes/Service/CropVariantsBuilder.php
+++ app/web/typo3conf/ext/theme/Classes/Service/CropVariantsBuilder.php
@@ -6,7 +6,10 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class CropVariantsBuilder
 {
-    protected $cropVariant = [];
+    /**
+     * @var array All eligible cropVariants
+     */
+    protected $cropVariants = [];
 
     protected $table = '';
 
@@ -28,12 +31,13 @@ class CropVariantsBuilder
 
     public function addCropVariants($cropVariant)
     {
-        $this->cropVariant[] = $cropVariant;
+        $this->cropVariants[] = $cropVariant;
         return $this;
     }
 
     public function removeDefaultCropVariants()
     {
+        // @TODO: TYPO3-Distribution: Retrieve default cropVariants and remove them from actual cropVariants array
         return $this;
     }
 
@@ -44,7 +48,7 @@ class CropVariantsBuilder
 
     public function persistToTca(string $customPath)
     {
-        // TODO:
+        // @TODO: TYPO3-Distribution: add persistToTca functionality
 //          a) Default, if no type is given:
 //              possible if crop is based on ChildTca's type: $GLOBALS['TCA']['a_table']['columns'][$this->fieldName]['config']['overrideChildTca']['types'][\TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE]['columnsOverrides']['crop']['config']['cropVariants'] = "set all cropVariants"
 //              better and easier AND most common: $GLOBALS['TCA'][$this->table]['columns'][$this->fieldName]['config']['overrideChildTca']['columns']['crop']['config']['cropVariants'] = "set all cropVariants"
@@ -53,5 +57,16 @@ class CropVariantsBuilder
 //          b) If a type is given:
 //
 //        $GLOBALS['TCA'][$this->table]['types'][$this->type]['columnsOverrides'][$this->fieldName]['config']['overrideChildTca']['columns']['crop']['config']['cropVariants'] = "set all cropVariants"
+
+        /*
+            What should/can the final array contain?
+                - cropVariants
+                    - title
+                    - cropArea
+                    - coverAreas
+                    - allowedAspectRatios
+                    - selectedRatio
+
+         */
     }
 }
