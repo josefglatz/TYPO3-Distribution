68e33dd477318fe9607e4a50751c520fcc6fd083
Josef Glatz <jousch@gmail.com>
2018-03-09 14:42:57 +0100

    [FEATURE] Add Development/Mamp configuration for MAMP PRO users
    By setting the following Apache directives it's possible to run TYPO3 within MAMP PRO:
    
    SetEnv TYPO3_CONTEXT "Development/Mamp"
    SetEnv MYSQL_USER "root"
    SetEnv MYSQL_PASSWORD "root"
    SetEnv MYSQL_DATABASE "projectDatabaseName"
    SetEnv MYSQL_PORT "8889"
    
    1. Install MAMP PRO
    2. Set up new host with correct PHP version
    3. Create new database
    4. Set the above mentioned EnvVars via Apache directives
    5. Install GraphicsMagick with "brew install graphicsmagick --with-ghostscript" (homebrow needs to be installed of course)
    
    Example on how to execute CLI:
    MYSQL_USER=distri MYSQL_PASSWORD=distri MYSQL_DATABASE=distri MYSQL_PORT=8889 TYPO3_CONTEXT=Development/Mamp /Applications/MAMP/bin/php/php7.1.12/bin/php ./typo3cms

---
+++ app/web/typo3conf/AdditionalConfiguration_Development_Mamp.php
@@ -0,0 +1,60 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+/*
+ * Example configuration block for MAMP's Apache host directives:
+ *
+ * Pre-requirements:
+ * - Create your database before installing/running project
+ * - Set your mysql server port if you use an alternative
+ */
+/*
+
+SetEnv TYPO3_CONTEXT "Development/Mamp"
+SetEnv MYSQL_USER "root"
+SetEnv MYSQL_PASSWORD "root"
+SetEnv MYSQL_DATABASE "projectDatabase"
+SetEnv MYSQL_PORT "8889"
+
+ */
+
+/*
+ * Run PHP CLI like:
+ */
+/*
+ *
+    MYSQL_USER=distri MYSQL_PASSWORD=distri MYSQL_DATABASE=distri MYSQL_PORT=8889 TYPO3_CONTEXT=Development/Mamp /Applications/MAMP/bin/php/php7.1.12/bin/php ./typo3cms
+
+ */
+
+// Override configuration of LocalConfiguration
+$customChanges = [
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'dbname' => getenv('MYSQL_DATABASE'),
+                'driver' => 'mysqli',
+                'host' => 'localhost',
+                'password' => getenv('MYSQL_PASSWORD'),
+                'port' => getenv('MYSQL_PORT'),
+                'user' => getenv('MYSQL_USER'),
+            ],
+        ],
+    ],
+    'GFX' => [
+        'colorspace' => 'RGB',
+        'processor' => 'GraphicsMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => -1,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/local/bin/',
+        'processor_path_lzw' => '/usr/local/bin/',
+    ],
+    'SYS' => [
+        'systemLocale' => 'de_DE.UTF-8',
+    ],
+];
+
+$GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive($GLOBALS['TYPO3_CONF_VARS'], $customChanges);
