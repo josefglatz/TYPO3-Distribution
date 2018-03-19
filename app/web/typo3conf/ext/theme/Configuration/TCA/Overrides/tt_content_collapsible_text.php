0a42e01d8ab62bc9fa81b13ac5caa03d206f3745
Josef Glatz <jousch@gmail.com>
2018-03-19 19:32:50 +0100

    [FEATURE] CE: New default content element type "theme_collapsible_text"
    - LLL: localized strings (EN, DE)
    - PageTSConfig: NCEW configuration
    - TCA configuration
    - TypoScript configuration
    - PageTSconfig: CKEditor preset assignment
    - PageTSConfig: limit header_layout items to common default

---
+++ app/web/typo3conf/ext/theme/Configuration/TCA/Overrides/tt_content_collapsible_text.php
@@ -0,0 +1,71 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+call_user_func(
+    function ($extKey, $table, $type) {
+        $languageFileCePrefix = 'LLL:EXT:theme/Resources/Private/Language/locallang_ContentElements.xlf:';
+
+        /**
+         * Add CE: stop inheritance
+         */
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+            $table,
+            'CType',
+            [
+                $languageFileCePrefix . $type . '.title',
+                $type,
+                'theme-content-accordion'
+            ],
+            'text',
+            'after'
+        );
+
+        $tca = [
+            'columns' => [
+
+            ],
+            'types' => [
+                $type => [
+                    'showitem' => '
+                        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                            header;' . $languageFileCePrefix . 'theme_collapsible_text.label.header,
+                            --linebreak--,
+                            header_layout;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_layout_formlabel,
+                            bodytext;' . $languageFileCePrefix . 'theme_collapsible_text.label.bodytext,
+                        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+                        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                            --palette--;;language,
+                        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                            --palette--;;hidden,
+                            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+                        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                            categories,
+                        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                            rowDescription,
+                        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+                    ',
+                    'columnsOverrides' => [
+                        'bodytext' => [
+                            'config' => [
+                                'enableRichtext' => true,
+                                'richtextConfiguration' => 'default'
+                            ]
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $GLOBALS['TCA'][$table] = array_replace_recursive($GLOBALS['TCA'][$table], $tca);
+
+        /**
+         * Assign Icon
+         */
+        $GLOBALS['TCA'][$table]['ctrl']['typeicon_classes'][$type] = 'theme-content-accordion';
+    },
+    'theme',
+    'tt_content',
+    'theme_collapsible_text'
+);
