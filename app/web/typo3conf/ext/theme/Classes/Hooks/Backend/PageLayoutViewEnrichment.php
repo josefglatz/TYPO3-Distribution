d188cb74e0f41be3e5a9c20cebb7cb471bb4cecd
Josef Glatz <jousch@gmail.com>
2018-03-20 07:11:09 +0100

    [WIP][FEATURE] PageLayoutViewEnrichment
    Introduce Class and editLink feature
    
    Related: #262

---
+++ app/web/typo3conf/ext/theme/Classes/Hooks/Backend/PageLayoutViewEnrichment.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types = 1);
+
+namespace JosefGlatz\Theme\Hooks\Backend;
+
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Backend\View\PageLayoutView;
+use TYPO3\CMS\Backend\View\PageLayoutViewDrawItemHookInterface;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Lang\LanguageService;
+
+class PageLayoutViewEnrichment implements PageLayoutViewDrawItemHookInterface
+{
+    /**
+     * @var IconFactory
+     */
+    protected $iconFactory;
+
+    public function __construct()
+    {
+        $this->iconFactory = GeneralUtility::makeInstance(IconFactory::class);
+    }
+
+    /** @noinspection MoreThanThreeArgumentsInspection
+     * @param PageLayoutView $parentObject
+     * @param $drawItem
+     * @param $headerContent
+     * @param $itemContent
+     * @param array $row
+     */
+    public function preProcess(PageLayoutView &$parentObject, &$drawItem, &$headerContent, &$itemContent, array &$row)
+    {
+        // General enrichments
+        $row['_extras']['editLink'] = $this->getEditLink($row);
+    }
+
+    /**
+     * CE edit link
+     *
+     * @param array $row
+     * @return string
+     */
+    protected function getEditLink(array $row): string
+    {
+        $url = '';
+        if ($this->getBackendUser()->recordEditAccessInternals('tt_content', $row)) {
+            $urlParameters = [
+                'edit' => [
+                    'tt_content' => [
+                        $row['uid'] => 'edit'
+                    ]
+                ],
+                'returnUrl' => GeneralUtility::getIndpEnv('REQUEST_URI') . '#element-tt_content-' . $row['uid'],
+            ];
+            $url = BackendUtility::getModuleUrl('record_edit', $urlParameters);
+        }
+        return $url;
+    }
+
+    /**
+     * @return LanguageService
+     */
+    protected function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
+    }
+
+    /**
+     * @return BackendUserAuthentication
+     */
+    protected function getBackendUser(): BackendUserAuthentication
+    {
+        return $GLOBALS['BE_USER'];
+    }
+}
