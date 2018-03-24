3e4477e945305e0802353a3b767c00f329e5185c
Josef Glatz <jousch@gmail.com>
2018-03-24 23:37:54 +0100

    [WIP][FEATURE] CKEditor plugin: DeleteAll
    - add inital custom plugin
    
    ToDo's:
    - language labels
    - icons (in different sizes)
    - finalize registered command "deleteAll"
    - register plugin in CKEditor configuration
    
    Related: #108

---
+++ app/web/typo3conf/ext/theme/Resources/Public/RTE/ProjectPlugins/deleteAll/plugin.js
@@ -0,0 +1,26 @@
+/**
+ * DeleteAll for CKEditor
+ */
+( function() {
+	CKEDITOR.plugins.add('deleteall', {
+		lang: 'de',
+		icons: 'deleteall',
+		hidpi: true,
+		init: function ( editor ) {
+			// create command "deleteAll" which deletes the content of the editor
+			editor.addCommand('deleteAll', { modes: {wysiwyg: 1, source: 1},
+				exec: function (editor) {
+					editor.setData('');
+				},
+				canUndo: false
+			} );
+
+			editor.ui.addButton && editor.ui.addButton( 'SelectAll', {
+				label: editor.lang.deleteall.DeleteButton,
+				command: 'deleteAll',
+				toolbar: 'selection,10',
+				icon: 'deleteall'
+			} );
+		}
+	} );
+} )();
