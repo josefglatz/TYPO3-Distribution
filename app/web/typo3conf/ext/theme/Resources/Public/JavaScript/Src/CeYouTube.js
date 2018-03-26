df785aca4e82b5c9b9ff7bf48cd414ed75a66391
Josef Glatz <jousch@gmail.com>
2018-03-26 22:22:33 +0200

    [FOLLOWUP][FEATURE] CE Youtube: Frontend Integration
    - CGL cleanup of initial copied frontend javascript
    
    Related: #288

--- app/web/typo3conf/ext/theme/Resources/Public/JavaScript/Src/CeYouTube.js
+++ app/web/typo3conf/ext/theme/Resources/Public/JavaScript/Src/CeYouTube.js
@@ -4,22 +4,22 @@
  * Script initially copied from t3brightside/youtubevideo
  */
 
-$( document ).ready(function() {
-	$('.youtubeVideo').each(function() {
-		if ($(this).width() < 600){
-			$(this).addClass( 'small' );
-		} else{
-			$(this).removeClass('small')
+$(document).ready(function () {
+	$('.youtubeVideo').each(function () {
+		if ($(this).width() < 600) {
+			$(this).addClass('small');
+		} else {
+			$(this).removeClass('small');
 		}
 	});
 });
 
-$(window).resize(function(){
-	$('.youtubeVideo').each(function() {
-		if ($(this).width() < 600){
-			$(this).addClass( 'small' );
-		} else{
-			$(this).removeClass('small')
+$(window).resize(function () {
+	$('.youtubeVideo').each(function () {
+		if ($(this).width() < 600) {
+			$(this).addClass('small');
+		} else {
+			$(this).removeClass('small');
 		}
 	});
 });
@@ -49,14 +49,13 @@ function getFrameID(id) {
 }
 
 // Define YT_ready function.
-var YT_ready = (function() {
+var YT_ready = (function () {
 	var onReady_funcs = [],
 		api_isReady = false;
-	/* @param func function     Function to execute on ready
+	  /* @param func function     Function to execute on ready
        * @param func Boolean      If true, all qeued functions are executed
-       * @param b_before Boolean  If true, the func will added to the first
-                                   position in the queue*/
-	return function(func, b_before) {
+       * @param b_before Boolean  If true, the func will added to the first position in the queue*/
+	return function (func, b_before) {
 		if (func === true) {
 			api_isReady = true;
 			for (var i = 0; i < onReady_funcs.length; i++) {
@@ -70,17 +69,18 @@ var YT_ready = (function() {
 		}
 	}
 })();
+
 // This function will be called when the API is fully loaded
 
 function onYouTubePlayerAPIReady() {
-	YT_ready(true)
+	YT_ready(true);
 }
 
 var players = {};
 //Define a player storage object, to enable later function calls,
 //  without having to create a new class instance again.
-YT_ready(function() {
-	$(".coverimage + iframe[id]").each(function() {
+YT_ready(function () {
+	$(".coverimage + iframe[id]").each(function () {
 		var identifier = this.id;
 		var frameID = getFrameID(identifier);
 		if (frameID) { //If the frame exists
@@ -98,16 +98,17 @@ YT_ready(function() {
 function createYTEvent(frameID, identifier) {
 	return function (event) {
 		var player = players[frameID]; // player object
-		var the_div = $('#'+identifier).parent();
-		the_div.children('.coverimage').click(function() {
+		var the_div = $('#' + identifier).parent();
+		the_div.children('.coverimage').click(function () {
 			var $this = $(this);
 			$this.fadeOut().next().addClass('play');
 			if ($this.next().hasClass('play')) {
 				player.playVideo();
 			}
 			setTimeout(doSomething, 700);
+
 			function doSomething() {
-				$('#'+frameID).show();
+				$('#' + frameID).show();
 			};
 		});
 	}
@@ -115,9 +116,10 @@ function createYTEvent(frameID, identifier) {
 
 
 // Load YouTube Frame API
-(function(){ //Closure, to not leak to the scope
+(function () { //Closure, to not leak to the scope
 	var s = document.createElement("script");
-	s.src = "https://www.youtube.com/player_api"; /* Load Player API*/
+	s.src = "https://www.youtube.com/player_api";
+	/* Load Player API*/
 	var before = document.getElementsByTagName("script")[0];
 	before.parentNode.insertBefore(s, before);
 })();
