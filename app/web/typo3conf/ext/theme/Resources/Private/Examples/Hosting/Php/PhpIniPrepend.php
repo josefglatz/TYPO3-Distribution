b708b0ea7bb4a942c720ca93ff6b20c0c97fcb01
Josef Glatz <jousch@gmail.com>
2018-03-28 12:32:43 +0200

    [DOCUMENTATION] Ready2use Mittwald shared hosting configuration snippets

--- app/web/typo3conf/ext/theme/Resources/Private/Examples/Hosting/Php/PhpIniPrepend.php
+++ app/web/typo3conf/ext/theme/Resources/Private/Examples/Hosting/Php/PhpIniPrepend.php
@@ -3,7 +3,7 @@ putenv('TYPO3_CONTEXT=Production/Live');
 putenv('TYPO3_CONTEXT=Production/Live/Frontend');
 putenv('TYPO3_CONTEXT=Production/Live/Backend');
 putenv('TYPO3_CONTEXT=Production/Dev');
-putenv('TYPO3_CONTEXT=Production/Stage');
+putenv('TYPO3_CONTEXT=Production/Staging');
 putenv('TYPO3_CONTEXT=Development/Test');
 header('X-Robots-Tag: noindex, nofollow, noarchive, noodp');
 
@@ -14,8 +14,13 @@ header('X-Robots-Tag: noindex, nofollow, noarchive, noodp');
  * auto_prepend_file = /home/www/pXXXXXX/html/prepend.php
  *
  * Create file on Mittwald:
- * `touch ~/html/prepend.php && echo -e "<?php\nputenv('TYPO3_CONTEXT=Production/Dev');\nheader('X-Robots-Tag: noindex, nofollow, noarchive');" >> ~/html/prepend.php`
+ *  touch ~/html/prepend.php && echo -e "<?php\nputenv('TYPO3_CONTEXT=Production/Dev');\nheader('X-Robots-Tag: noindex, nofollow, noarchive');" >> ~/html/prepend.php
+ *  touch ~/html/prepend.php && echo -e "<?php\nputenv('TYPO3_CONTEXT=Production/Staging');\nheader('X-Robots-Tag: noindex, nofollow, noarchive');" >> ~/html/prepend.php
+ *  touch ~/html/prepend.php && echo -e "<?php\nputenv('TYPO3_CONTEXT=Production/Live');" >> ~/html/prepend.php
+ *  touch ~/html/prepend.php && echo -e "<?php\nputenv('TYPO3_CONTEXT=Production/Live/Frontend');" >> ~/html/prepend.php
+ *  touch ~/html/prepend.php && echo -e "<?php\nputenv('TYPO3_CONTEXT=Production/Live/Backend');" >> ~/html/prepend.php
  *
  * Change php option via command in php.ini:
- * `sed -i "s|auto_prepend_file =|auto_prepend_file = /home/www/pXXXXXX/html/prepend.php|g" /etc/php/php.ini`
+ * sed -i "s|auto_prepend_file =|auto_prepend_file = /home/www/$USER/html/prepend.php|g" /etc/php/php.ini
+ *
  */
