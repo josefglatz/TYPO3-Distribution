3afc680287624ced407e9440a370d01e0634e37f
Josef Glatz <jousch@gmail.com>
2018-04-12 07:33:21 +0200

    [FEATURE] EXT:news: TCA/Overrides: feature flag "limitFalMediaToImagefileExt"
    Limit tx_news_domain_model_news.fal_media to imagefile extensions by setting
    $limitFalMediaToImagefileExt in TCA/Overrides/tx_news_domain_model_news.php
    to true

--- app/web/typo3conf/ext/theme/Configuration/TCA/Overrides/tx_news_domain_model_news.php
+++ app/web/typo3conf/ext/theme/Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -6,6 +6,11 @@ call_user_func(
         $languageFileBePrefix = 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_BackendGeneral.xlf:';
 
         /***************
+         * feature flags
+         */
+        $limitFalMediaToImagefileExt = false;
+
+        /***************
          * Apply changes only if EXT:news is loaded
          */
         if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')) {
@@ -24,6 +29,42 @@ call_user_func(
 
             ];
             \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $additionalColumns);
+
+            /***************
+             * CE feature flags
+             */
+            if ($limitFalMediaToImagefileExt) {
+                $GLOBALS['TCA'][$table]['columns'] = array_replace_recursive(
+                    $GLOBALS['TCA'][$table]['columns'],
+                    [
+                        'fal_media' => [
+                            'config' => [
+                                'overrideChildTca' => [
+                                    'columns' => [
+                                        'uid_local' => [
+                                            'config' => [
+                                                'appearance' => [
+                                                    'elementBrowserType' => 'file',
+                                                    'elementBrowserAllowed' => $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'],
+                                                ],
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                                'filter' => [
+                                    [
+                                        'userFunc' => \TYPO3\CMS\Core\Resource\Filter\FileExtensionFilter::class . '->filterInlineChildren',
+                                        'parameters' => [
+                                            'allowedFileExtensions' => $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'],
+                                            'disallowedFileExtensions' => ''
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ]
+                );
+            }
 
             /***************
              * Configure cropVariants
