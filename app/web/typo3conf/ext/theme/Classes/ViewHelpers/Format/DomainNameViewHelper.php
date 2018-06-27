d84c136ad6ab3f317fce4ffe73b2d5176e6c79b7
Josef Glatz <jousch@gmail.com>
2018-06-27 14:29:51 +0200

    [TASK] DomainNameVH: use full-qualified function call
    a micro-performance-optimization task

--- app/web/typo3conf/ext/theme/Classes/ViewHelpers/Format/DomainNameViewHelper.php
+++ app/web/typo3conf/ext/theme/Classes/ViewHelpers/Format/DomainNameViewHelper.php
@@ -50,7 +50,7 @@ class DomainNameViewHelper extends AbstractViewHelper implements CompilableInter
         $url = substr($url, 0, 4) === 'http' ? $url : 'http://' . $url;
         $d = parse_url($url);
         $tmp = explode('.', $d['host']);
-        $n = count($tmp);
+        $n = \count($tmp);
         if ($n >= 2) {
             if ($n == 4 || ($n == 3 && strlen($tmp[($n - 2)]) <= 3)) {
                 $d['domain'] = $tmp[($n - 3)] . '.' . $tmp[($n - 2)] . '.' . $tmp[($n - 1)];
