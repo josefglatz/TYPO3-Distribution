709702dc1631a868af5d2737361ef933e5d553a0
Josef Glatz <jousch@gmail.com>
2018-11-19 21:49:04 +0100

    [FEATURE] Add new ApplicationContext specific ViewHelpers
    - {theme:context:get()} returns string
    - {theme:context:productionLive()} returns bool

---
+++ app/web/typo3conf/ext/theme/Classes/ViewHelpers/Context/GetViewHelper.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types = 1);
+
+namespace JosefGlatz\Theme\ViewHelpers\Context;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+/**
+ * ### Context: Get
+ *
+ * Returns the current application context which may include possible sub-contexts.
+ * The application context can be 'Production', 'Development' or 'Testing'.
+ * Additionally each context can be extended with custom sub-contexts like: 'Production/Staging' or
+ * 'Production/Staging/Server1'. If no application context has been set by the configuration, then the
+ * default context is 'Production'.
+ *
+ * #### Note about how to set the application context
+ *
+ * The context TYPO3 CMS runs in is specified through the environment variable TYPO3_CONTEXT.
+ * It can be set by .htaccess or in the server configuration
+ *
+ * See: http://docs.typo3.org/typo3cms/CoreApiReference/ApiOverview/Bootstrapping/Index.html#bootstrapping-context
+ */
+class GetViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): string
+    {
+        return (string) GeneralUtility::getApplicationContext();
+    }
+}
---
+++ app/web/typo3conf/ext/theme/Classes/ViewHelpers/Context/ProductionLiveViewHelper.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace JosefGlatz\Theme\ViewHelpers\Context;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\StringUtility;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+/**
+ * ### Context: ProductionLive
+ *
+ * Returns true if the application context is Production/Live explicitly.
+ *
+ * #### Note about how to set the application context
+ *
+ * The context TYPO3 CMS runs in is specified through the environment variable TYPO3_CONTEXT.
+ * It can be set by .htaccess or in the server configuration
+ *
+ * See: http://docs.typo3.org/typo3cms/CoreApiReference/ApiOverview/Bootstrapping/Index.html#bootstrapping-context
+ */
+class ProductionLiveViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return bool
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): bool
+    {
+        return (bool) StringUtility::beginsWith(GeneralUtility::getApplicationContext()->__toString(), 'Production/Live');
+    }
+}
