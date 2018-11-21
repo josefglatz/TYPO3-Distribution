140e635fefd5a86c362b61c0c9e4bf1d4ddeff80
Josef Glatz <jousch@gmail.com>
2018-11-21 20:12:34 +0100

    [TASK] Update TYPO3 version constraint
    Related: #340

--- app/web/typo3conf/ext/theme/ext_emconf.php
+++ app/web/typo3conf/ext/theme/ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '8.7.1-9.5.99',
+                    'typo3' => '9.5.0-9.5.99',
                 ],
             'conflicts' =>
                 [
