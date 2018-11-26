6ce83302ba75f12c60854e5a4aba712f3c0afc5d
Josef Glatz <jousch@gmail.com>
2018-11-26 07:16:28 +0100

    [TASK] Revision theme:debug.typoScript ViewHelper
    Related: #370

--- app/web/typo3conf/ext/theme/Classes/ViewHelpers/Debug/TypoScriptViewHelper.php
+++ app/web/typo3conf/ext/theme/Classes/ViewHelpers/Debug/TypoScriptViewHelper.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types = 1);
 
 namespace JosefGlatz\Theme\ViewHelpers\Debug;
 
@@ -7,25 +8,41 @@ use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
 use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
-use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
 class TypoScriptViewHelper extends AbstractViewHelper
 {
+    use CompileWithRenderStatic;
+
     /**
-     * @throws \InvalidArgumentException
+     * Output of this viewhelper is already escaped
+     *
+     * @var bool
+     */
+    protected $escapeOutput = false;
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string
      * @throws \TYPO3\CMS\Extbase\Configuration\Exception\InvalidConfigurationTypeException
      */
-    public function render()
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): string
     {
         if (GeneralUtility::getApplicationContext()->isDevelopment()) {
             $extbaseFrameworkConfiguration = GeneralUtility::makeInstance(ObjectManager::class)
                 ->get(ConfigurationManager::class)
                 ->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
-            DebuggerUtility::var_dump(
+            return DebuggerUtility::var_dump(
                 $extbaseFrameworkConfiguration,
-                'CONFIGURATION_TYPE_FULL_TYPOSCRIPT || Use the Adminpanel to view the final rendered TypoScript!',
+                'CONFIGURATION_TYPE_FULL_TYPOSCRIPT || Use the admin panel to view the final rendered TypoScript!',
                 500
             );
         }
+
+        return '';
     }
 }
