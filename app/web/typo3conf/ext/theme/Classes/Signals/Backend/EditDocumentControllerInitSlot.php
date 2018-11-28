fb52c8734d3fdece0890bebd9d2d37725a5f3e5c
Josef Glatz <jousch@gmail.com>
2018-11-28 11:07:43 +0100

    [TASK] Update EditDocumentController preInitAfter SignalSlot usage
    No deprecated property is used by the signal slot method
    anymore.
    
    Fixes: #347

--- app/web/typo3conf/ext/theme/Classes/Signals/Backend/EditDocumentControllerInitSlot.php
+++ app/web/typo3conf/ext/theme/Classes/Signals/Backend/EditDocumentControllerInitSlot.php
@@ -8,6 +8,7 @@ use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Messaging\FlashMessageService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\HttpUtility;
 
 /**
  * Enrich/Customize EditDocumentController via SignalSlot usage
@@ -18,36 +19,41 @@ class EditDocumentControllerInitSlot
      * preInitAfter SignalSlot
      *   - restrict creating sys_template records in database
      *
-     * @TODO: Use the ServerRequestInterface to interception
-     *
      * @param EditDocumentController $editDocumentController
+     * @param ServerRequestInterface $request
      */
     public function adjustEditDocumentController(EditDocumentController $editDocumentController, ServerRequestInterface $request): void
     {
-        $editconf = $editDocumentController->editconf;
-
-        if (isset($editconf['sys_template'])) {
-            if (reset(reset($editconf)) === 'new') {
-                /**
-                 * @var FlashMessage $message Error message to inform the backend user about the barrier
-                 */
-                $message = GeneralUtility::makeInstance(
-                    FlashMessage::class,
-                    htmlspecialchars($this->getLanguageService()
-                        ->sL('LLL:EXT:theme/Resources/Private/Language/locallang_BackendGeneral.xlf:hooks.dataHandler.prevent.sys_template.description')),
-                    htmlspecialchars($this->getLanguageService()
-                        ->sL('LLL:EXT:theme/Resources/Private/Language/locallang_BackendGeneral.xlf:hooks.dataHandler.prevent.sys_template.title')),
-                    FlashMessage::ERROR,
-                    true
-                );
-                $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
-                $messageQueue = $flashMessageService->getMessageQueueByIdentifier();
-                $messageQueue->addMessage($message);
-
-                // Return to previous url without saving anything
-                $editDocumentController->closeDocument();
-            }
+        $parsedBody = $request->getParsedBody();
+        $queryParams = $request->getQueryParams();
+        $parsedRequestParams = $parsedBody['edit']['sys_template'] ?? $queryParams['edit']['sys_template'] ?? [];
+        if (!empty($parsedRequestParams) && \in_array('new', $parsedRequestParams, true)
+        ) {
+            $this->addFlashMessage();
+            HttpUtility::redirect($queryParams['returnUrl'], HttpUtility::HTTP_STATUS_403);
         }
+    }
+
+    /**
+     * Add flash message to queue to inform the TYPO3 backend administrator about the restriction.
+     */
+    protected function addFlashMessage(): void
+    {
+        /**
+         * @var FlashMessage $message Error message to inform the backend user about the barrier
+         */
+        $message = GeneralUtility::makeInstance(
+            FlashMessage::class,
+            htmlspecialchars($this->getLanguageService()
+                ->sL('LLL:EXT:theme/Resources/Private/Language/locallang_BackendGeneral.xlf:hooks.dataHandler.prevent.sys_template.description')),
+            htmlspecialchars($this->getLanguageService()
+                ->sL('LLL:EXT:theme/Resources/Private/Language/locallang_BackendGeneral.xlf:hooks.dataHandler.prevent.sys_template.title')),
+            FlashMessage::ERROR,
+            true
+        );
+        $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
+        $messageQueue = $flashMessageService->getMessageQueueByIdentifier();
+        $messageQueue->addMessage($message);
     }
 
     /**
