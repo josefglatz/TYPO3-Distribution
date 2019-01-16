f5042f7982fd34c01bb3ad9d77618f9ccde32f89
Josef Glatz <jousch@gmail.com>
2019-01-16 15:51:26 +0100

    [BUGFIX] Fix shell script for importing pages

--- app/Build/InstallDefaultDatabaseRecords.sh
+++ app/Build/InstallDefaultDatabaseRecords.sh
@@ -8,17 +8,17 @@
 echo "Begin importing default database records"
 echo " "
 echo " "
-/app/typo3cms import:table 'sys_be_shortcuts' --match-fields='uid'
-/app/typo3cms import:table 'tx_scheduler_task_group' --match-fields='uid'
-/app/typo3cms import:table 'tx_scheduler_task' --match-fields='uid'
-/app/typo3cms import:table 'sys_filemounts' --match-fields='uid'
-/app/typo3cms import:table 'be_groups' --match-fields='uid'
-/app/typo3cms import:table 'be_users' --match-fields='uid'
-/app/typo3cms import:table 'pages' --match-fields='uid'
-/app/typo3cms import:table 'tt_content' --match-fields='uid'
-/app/typo3cms import:table 'fe_groups' --match-fields='uid'
-/app/typo3cms import:table 'fe_users' --match-fields='uid'
-/app/typo3cms import:table 'tt_content' --match-fields='uid'
+/app/typo3cms import:table 'sys_be_shortcuts' --matchFields='uid'
+/app/typo3cms import:table 'tx_scheduler_task_group' --matchFields='uid'
+/app/typo3cms import:table 'tx_scheduler_task' --matchFields='uid'
+/app/typo3cms import:table 'sys_filemounts' --matchFields='uid'
+/app/typo3cms import:table 'be_groups' --matchFields='uid'
+/app/typo3cms import:table 'be_users' --matchFields='uid'
+/app/typo3cms import:table 'pages' --matchFields='uid'
+/app/typo3cms import:table 'tt_content' --matchFields='uid'
+/app/typo3cms import:table 'fe_groups' --matchFields='uid'
+/app/typo3cms import:table 'fe_users' --matchFields='uid'
+/app/typo3cms import:table 'tt_content' --matchFields='uid'
 echo " "
 echo " "
 echo "Finalized the import of default database records"
