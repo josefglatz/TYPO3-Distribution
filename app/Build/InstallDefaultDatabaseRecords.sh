f098a6b6a0c403903b70a75a82b66cd731202974
Josef Glatz <josefglatz@gmail.com>
2020-02-10 07:57:06 +0100

    [TASK] Add support for yaml_configuration task/9-5_lts branch

--- app/Build/InstallDefaultDatabaseRecords.sh
+++ app/Build/InstallDefaultDatabaseRecords.sh
@@ -8,17 +8,17 @@
 echo "Begin importing default database records"
 echo " "
 echo " "
-/app/typo3cms import:table 'sys_be_shortcuts' --matchFields='uid'
-/app/typo3cms import:table 'tx_scheduler_task_group' --matchFields='uid'
-/app/typo3cms import:table 'tx_scheduler_task' --matchFields='uid'
-/app/typo3cms import:table 'sys_filemounts' --matchFields='uid'
-/app/typo3cms import:table 'be_groups' --matchFields='uid'
-/app/typo3cms import:table 'be_users' --matchFields='uid'
-/app/typo3cms import:table 'pages' --matchFields='uid'
-/app/typo3cms import:table 'tt_content' --matchFields='uid'
-/app/typo3cms import:table 'fe_groups' --matchFields='uid'
-/app/typo3cms import:table 'fe_users' --matchFields='uid'
-/app/typo3cms import:table 'tt_content' --matchFields='uid'
+/app/typo3cms yaml:import 'sys_be_shortcuts' --matchFields='uid'
+/app/typo3cms yaml:import 'tx_scheduler_task_group' --matchFields='uid'
+/app/typo3cms yaml:import 'tx_scheduler_task' --matchFields='uid'
+/app/typo3cms yaml:import 'sys_filemounts' --matchFields='uid'
+/app/typo3cms yaml:import 'be_groups' --matchFields='uid'
+/app/typo3cms yaml:import 'be_users' --matchFields='uid'
+/app/typo3cms yaml:import 'pages' --matchFields='uid'
+/app/typo3cms yaml:import 'tt_content' --matchFields='uid'
+/app/typo3cms yaml:import 'fe_groups' --matchFields='uid'
+/app/typo3cms yaml:import 'fe_users' --matchFields='uid'
+/app/typo3cms yaml:import 'tt_content' --matchFields='uid'
 echo " "
 echo " "
 echo "Finalized the import of default database records"
